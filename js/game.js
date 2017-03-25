89ce59269a65aed61346b4a5dd4180b3b24f7a11
Jay Schwane <jrschwane@uwalumni.com>
2017-03-25 10:06:29 -0500

    Game features complete; Needs refactoring / cleanup

--- js/game.js
+++ js/game.js
@@ -7,7 +7,8 @@ var Game = (function ($) {
 
     var DOM = {},        // populated by cacheDom()
         
-        len = 20,        // number of turns to win        
+        len = 5,        // number of turns to win    
+        strict = false,
         sequence = [],
         
         playerTurn = false,
@@ -28,56 +29,78 @@ var Game = (function ($) {
 
     // cache DOM elements
     function cacheDom() {
-        DOM.$game       = $('#game');
-        DOM.$board      = DOM.$game.find('.board');
-        DOM.$strictBtn  = DOM.$board.find('#strict-btn');
-        DOM.$resetBtn   = DOM.$board.find('#reset-btn');
-
-        DOM.$btn1       = DOM.$board.find('#btn1');
-        DOM.$btn2       = DOM.$board.find('#btn2');
-        DOM.$btn3       = DOM.$board.find('#btn3');
-        DOM.$btn4       = DOM.$board.find('#btn4');
-
-        DOM.$score      = DOM.$game.find('#score');
-
-        DOM.$readyModal = $(document.createElement('div'));
-        DOM.$readyBtn   = $(document.createElement('button'));
         
-        DOM.$victorModal = $(document.createElement('div'));        
-        DOM.$victorBtn   = $(document.createElement('button'));
+        /* game framework elements */
+        DOM.$game        = $('#game');
+        DOM.$board       = DOM.$game.find('.board');
+        DOM.$strictBtn   = DOM.$board.find('#strict-btn');
+        DOM.$resetBtn    = DOM.$board.find('#reset-btn');
+
+        /* game buttons */
+        DOM.$btn1        = DOM.$board.find('#btn1');
+        DOM.$btn2        = DOM.$board.find('#btn2');
+        DOM.$btn3        = DOM.$board.find('#btn3');
+        DOM.$btn4        = DOM.$board.find('#btn4');
+
+        DOM.$score       = DOM.$game.find('#score');
+
+        /* modals */
+        DOM.$readyModal  = $(document.createElement('div'));
+        DOM.$errorModal  = $(document.createElement('div'));
+        DOM.$victorModal = $(document.createElement('div'));
+        DOM.$readyBtn    = $(document.createElement('button'));
     }
 
 
     // bind events
     function bindEvents() {
         DOM.$game.on('click', '#ready-btn', startGame);
+        DOM.$game.on('click', '#play-again-btn', resetGame);
         DOM.$resetBtn.on('click', resetGame);
+        DOM.$strictBtn.on('click', toggleStrict);
         DOM.$board.on('click', gameBtnClick);
     }
     
     
-    // start game
+    /** start game
+     *
+     * @params   [object]   e   [click event]
+    */
     function startGame(e) {
-        
         DOM.$readyModal.hide();
-//        resetGame();
         advanceTurn();
-        
         e.stopPropagation();
-        
     }
     
     
-    // handle main game button click events
+    /** toggle strict mode
+     *
+     * @params   [object]   e   [click event]
+    */
+    function toggleStrict(e) {
+        console.log('strict clicked. ' + strict);
+        strict = !strict;
+        DOM.$strictBtn
+            .toggleClass('red', strict);
+        e.stopPropagation();
+    }
+    
+    
+    /** handle main game button click events
+     *
+     * @params   [object]   e   [click event]
+    */
     function gameBtnClick(e) {
         
-        var num = (e.target.id).slice(3);
+        var id  = (e.target.id).slice(3),
+            num = parseInt(id, 10);
         
+        // only take action on the numbered buttons
         if (/\d/.test(num)) {
             
             playOne(num);
-            playerSequence.push(parseInt(num, 10));
-            evalPlayerSequence();
+            playerSequence.push(num);
+            evalPlayerTurn(num);
                         
         }
         
@@ -137,61 +160,79 @@ var Game = (function ($) {
     }
     
     
-    // check if sequences have same length
+    /** check if sequences have same length
+     *
+     * @params   [array]   seqA   [sequence A]
+     * @params   [array]   seqB   [sequence B]
+     * @returns  [boolean]
+    */
     function sameLength(seqA, seqB) {
         return seqA.length === seqB.length;
     }
     
     
-    // check if sequences have same content
-    function sameContent(seqA, seqB) {
+    // evaluate player's turn
+    function evalPlayerTurn(play) {
         
-        // fail if arrays are not same length
-        if (seqA.length !== seqB.length) {
-            return false;
-        }
-        
-        // fail if arrays do not contain the same elements
-        for (let i = 0; i < seqA.length; i += 1) {
-            if (seqA[i] !== seqB[i]) {
-                return false;
+        var index = playerSequence.length - 1;
+
+        // if player's move matches computer's move
+        if (play === sequence[index]) {
+            
+            // check for win or advance turn
+            evalPlayerSequence();
+            
+        } else {
+            
+            // otherwise, file mode depends on strict mode
+            if (strict) {
+                
+                // if strict, reset game
+                errorModal('YOU LOSE');
+                setTimeout(resetGame, 1500);
+                
+            } else {
+                
+                // if not strict, replay current turn
+                errorModal('Try Again');
+                playerSequence.length = 0;
+                playerTurn = false;
+                setTimeout(playSequence, 1500);
+                
             }
+            
         }
         
-        // otherwise pass
-        return true;
     }
     
     
-    // evaluate player's inputs
+    // evaluate player sequence for win or turn advance
     function evalPlayerSequence() {
         
-        console.log(' === Player\'s Turn === ');
-        console.log('sequence: ', playerSequence);
-        
-        // get subset of whole sequence
+        // get current turn's subset of full sequence
         var computerSequence = sequence.slice(0, turn);
         
-        // check that sequences are the same length
-        if (sameLength(playerSequence, computerSequence)) {
+        // check for win
+        if (sameLength(playerSequence, sequence)) {
             
-            // check that sequences contain the same elements
-            if (!sameContent(playerSequence, computerSequence)) {
-                
-                console.log(' ******* you fucked up! ******* ');
+            victoryModal();
             
-            }
+        } else {
             
-            console.log('Good job!');
-            turn += 1;
-            playerTurn = !playerTurn;
-            playerSequence.length = 0;
-            
-            setTimeout(advanceTurn, 750);
+            // advance turn if sequences are the same length
+            if (sameLength(playerSequence, computerSequence)) {
 
-            
+                turn += 1;
+                playerTurn = !playerTurn;
+                playerSequence.length = 0;
+
+                setTimeout(advanceTurn, 750);
+
+
+            }
+
         }
-        
+                
     }
     
     
@@ -212,7 +253,8 @@ var Game = (function ($) {
         }
                 
         // temp display sequence on screen
-        DOM.$score.html('turn: ' + turn +
+        DOM.$score.html('strict: ' + strict +
+                        ' | turn: ' + turn +
                         ' | player turn: ' + playerTurn);
         
     }
@@ -228,10 +270,28 @@ var Game = (function ($) {
         // reset game state
         playerTurn = false;
         sequence = generateSequence(len);
+        playerSequence.length = 0;
         turn = 1;
         step = 0;
         
         readyModal();
+    }
+    
+    
+    // error modal
+    function errorModal(error) {
+        
+        DOM.$errorModal
+            .addClass('modal')
+            .appendTo(DOM.$game)
+            .html(`<p>${error}</p>`)
+            .show();
+        
+        setTimeout(function () {
+            DOM.$errorModal
+                .hide();
+        }, 1500);
+        
     }
     
     
@@ -251,6 +311,28 @@ var Game = (function ($) {
             .addClass('modal')
             .appendTo(DOM.$game)
             .html(`<p>I am</p>`)
+            .append(group)
+            .show();
+    }
+    
+    
+    // victory modal
+    // initial modal for ready player go!
+    function victoryModal() {
+        
+        var group = $(document.createElement('div'));
+        
+        DOM.$readyBtn
+            .addClass('ready-btn')
+            .attr('id', 'play-again-btn')
+            .attr('type', 'button')
+            .text('Play Again!')
+            .appendTo(group);
+        
+        DOM.$readyModal
+            .addClass('modal')
+            .appendTo(DOM.$game)
+            .html(`<p>YOU WIN</p>`)
             .append(group)
             .show();
     }
